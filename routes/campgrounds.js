25835feed7194e0fa02a40b252c639a65c0022f9
Kayzee <iwp.malaysia@gmail.com>
2021-01-16 21:59:02 +0800

    added router.route

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,27 +1,23 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 const campgrounds = require("../controllers/campgrounds");
-
-//*import Express Error
 const catchAsync = require("../utilities/catchAsync");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
 
-//* routes
-
-router.get("/", catchAsync(campgrounds.index));
+router
+  .route("/")
+  .get(catchAsync(campgrounds.index))
+  .post(validateCampground, isLoggedIn, catchAsync(campgrounds.createCampground));
 
 //! this route is above the campground/id route so that it can work, if not the word "new" will be treated as an ID
-
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
-router.post("/", validateCampground, isLoggedIn, catchAsync(campgrounds.createCampground));
+router
+  .route("/:id")
+  .get(catchAsync(campgrounds.showCampground))
+  .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+  .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
-router.get("/:id", catchAsync(campgrounds.showCampground));
-
-//*route for edit & update campgrounds
 router.get("/:id/edit", isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
-router.put("/:id", isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
-
-router.delete("/:id", isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 module.exports = router;
